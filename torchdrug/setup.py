02131c1d18a42f14b268c6dd8bf6e3e85c5d4d4f
bivekpok <144483051+bivekpok@users.noreply.github.com>
2025-08-16 23:53:02 -0400

    Create setup.py

---
+++ torchdrug/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="torchdrug-local",  # Unique name to avoid PyPI conflicts
+    version="0.1.0",         # Match your modified version
+    packages=find_packages(),
+)
